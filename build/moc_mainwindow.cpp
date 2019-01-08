cd3a85f1e935e65163757cd0a10ca264391b4fb7
F4HTB <olivier@f4htb.fr>
2019-01-08 17:44:29 +0000

    second

--- build/moc_mainwindow.cpp
+++ build/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[27];
-    char stringdata0[485];
+    QByteArrayData data[43];
+    char stringdata0[723];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -41,21 +41,37 @@ QT_MOC_LITERAL(8, 123, 20), // "on_toggleLSB_toggled"
 QT_MOC_LITERAL(9, 144, 20), // "on_toggleRun_toggled"
 QT_MOC_LITERAL(10, 165, 24), // "on_spinFreq_valueChanged"
 QT_MOC_LITERAL(11, 190, 3), // "val"
-QT_MOC_LITERAL(12, 194, 15), // "tmrRead_timeout"
-QT_MOC_LITERAL(13, 210, 8), // "setShift"
-QT_MOC_LITERAL(14, 219, 15), // "on_shiftChanged"
-QT_MOC_LITERAL(15, 235, 9), // "newOffset"
-QT_MOC_LITERAL(16, 245, 26), // "on_spinOffset_valueChanged"
-QT_MOC_LITERAL(17, 272, 4), // "arg1"
-QT_MOC_LITERAL(18, 277, 26), // "on_spinCenter_valueChanged"
-QT_MOC_LITERAL(19, 304, 26), // "on_spinSlider_valueChanged"
-QT_MOC_LITERAL(20, 331, 26), // "on_RTLGSlider_valueChanged"
-QT_MOC_LITERAL(21, 358, 18), // "on_RTLHAGC_toggled"
-QT_MOC_LITERAL(22, 377, 18), // "on_RTLSAGC_toggled"
-QT_MOC_LITERAL(23, 396, 17), // "on_UPConv_toggled"
-QT_MOC_LITERAL(24, 414, 38), // "on_comboDirectSamp_currentInd..."
-QT_MOC_LITERAL(25, 453, 5), // "index"
-QT_MOC_LITERAL(26, 459, 25) // "on_toggleTransmit_toggled"
+QT_MOC_LITERAL(12, 194, 15), // "on_VFOH_pressed"
+QT_MOC_LITERAL(13, 210, 15), // "on_VFOK_pressed"
+QT_MOC_LITERAL(14, 226, 15), // "on_VFOM_pressed"
+QT_MOC_LITERAL(15, 242, 15), // "on_VFOG_pressed"
+QT_MOC_LITERAL(16, 258, 14), // "on_FQH_pressed"
+QT_MOC_LITERAL(17, 273, 14), // "on_FQK_pressed"
+QT_MOC_LITERAL(18, 288, 14), // "on_FQM_pressed"
+QT_MOC_LITERAL(19, 303, 14), // "on_FQG_pressed"
+QT_MOC_LITERAL(20, 318, 14), // "on_OFH_pressed"
+QT_MOC_LITERAL(21, 333, 14), // "on_OFK_pressed"
+QT_MOC_LITERAL(22, 348, 14), // "on_OFM_pressed"
+QT_MOC_LITERAL(23, 363, 16), // "on_FP100_pressed"
+QT_MOC_LITERAL(24, 380, 15), // "on_FP10_pressed"
+QT_MOC_LITERAL(25, 396, 14), // "on_FP1_pressed"
+QT_MOC_LITERAL(26, 411, 16), // "on_FM100_pressed"
+QT_MOC_LITERAL(27, 428, 15), // "on_FM10_pressed"
+QT_MOC_LITERAL(28, 444, 14), // "on_FM1_pressed"
+QT_MOC_LITERAL(29, 459, 15), // "tmrRead_timeout"
+QT_MOC_LITERAL(30, 475, 8), // "setShift"
+QT_MOC_LITERAL(31, 484, 15), // "on_shiftChanged"
+QT_MOC_LITERAL(32, 500, 9), // "newOffset"
+QT_MOC_LITERAL(33, 510, 26), // "on_spinOffset_valueChanged"
+QT_MOC_LITERAL(34, 537, 4), // "arg1"
+QT_MOC_LITERAL(35, 542, 26), // "on_spinCenter_valueChanged"
+QT_MOC_LITERAL(36, 569, 26), // "on_RTLGSlider_valueChanged"
+QT_MOC_LITERAL(37, 596, 18), // "on_RTLHAGC_toggled"
+QT_MOC_LITERAL(38, 615, 18), // "on_RTLSAGC_toggled"
+QT_MOC_LITERAL(39, 634, 17), // "on_UPConv_toggled"
+QT_MOC_LITERAL(40, 652, 38), // "on_comboDirectSamp_currentInd..."
+QT_MOC_LITERAL(41, 691, 5), // "index"
+QT_MOC_LITERAL(42, 697, 25) // "on_toggleTransmit_toggled"
 
     },
     "MainWindow\0on_toggleWFM_toggled\0\0"
@@ -63,11 +79,18 @@ QT_MOC_LITERAL(26, 459, 25) // "on_toggleTransmit_toggled"
     "on_toggleAM_toggled\0on_toggleCW_toggled\0"
     "on_toggleUSB_toggled\0on_toggleLSB_toggled\0"
     "on_toggleRun_toggled\0on_spinFreq_valueChanged\0"
-    "val\0tmrRead_timeout\0setShift\0"
+    "val\0on_VFOH_pressed\0on_VFOK_pressed\0"
+    "on_VFOM_pressed\0on_VFOG_pressed\0"
+    "on_FQH_pressed\0on_FQK_pressed\0"
+    "on_FQM_pressed\0on_FQG_pressed\0"
+    "on_OFH_pressed\0on_OFK_pressed\0"
+    "on_OFM_pressed\0on_FP100_pressed\0"
+    "on_FP10_pressed\0on_FP1_pressed\0"
+    "on_FM100_pressed\0on_FM10_pressed\0"
+    "on_FM1_pressed\0tmrRead_timeout\0setShift\0"
     "on_shiftChanged\0newOffset\0"
     "on_spinOffset_valueChanged\0arg1\0"
     "on_spinCenter_valueChanged\0"
-    "on_spinSlider_valueChanged\0"
     "on_RTLGSlider_valueChanged\0"
     "on_RTLHAGC_toggled\0on_RTLSAGC_toggled\0"
     "on_UPConv_toggled\0"
@@ -82,7 +105,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      20,   14, // methods
+      36,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -90,26 +113,42 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,  114,    2, 0x08 /* Private */,
-       4,    1,  117,    2, 0x08 /* Private */,
-       5,    1,  120,    2, 0x08 /* Private */,
-       6,    1,  123,    2, 0x08 /* Private */,
-       7,    1,  126,    2, 0x08 /* Private */,
-       8,    1,  129,    2, 0x08 /* Private */,
-       9,    1,  132,    2, 0x08 /* Private */,
-      10,    1,  135,    2, 0x08 /* Private */,
-      12,    0,  138,    2, 0x08 /* Private */,
-      13,    0,  139,    2, 0x08 /* Private */,
-      14,    1,  140,    2, 0x08 /* Private */,
-      16,    1,  143,    2, 0x08 /* Private */,
-      18,    1,  146,    2, 0x08 /* Private */,
-      19,    1,  149,    2, 0x08 /* Private */,
-      20,    1,  152,    2, 0x08 /* Private */,
-      21,    1,  155,    2, 0x08 /* Private */,
-      22,    1,  158,    2, 0x08 /* Private */,
-      23,    1,  161,    2, 0x08 /* Private */,
-      24,    1,  164,    2, 0x08 /* Private */,
-      26,    1,  167,    2, 0x08 /* Private */,
+       1,    1,  194,    2, 0x08 /* Private */,
+       4,    1,  197,    2, 0x08 /* Private */,
+       5,    1,  200,    2, 0x08 /* Private */,
+       6,    1,  203,    2, 0x08 /* Private */,
+       7,    1,  206,    2, 0x08 /* Private */,
+       8,    1,  209,    2, 0x08 /* Private */,
+       9,    1,  212,    2, 0x08 /* Private */,
+      10,    1,  215,    2, 0x08 /* Private */,
+      12,    0,  218,    2, 0x08 /* Private */,
+      13,    0,  219,    2, 0x08 /* Private */,
+      14,    0,  220,    2, 0x08 /* Private */,
+      15,    0,  221,    2, 0x08 /* Private */,
+      16,    0,  222,    2, 0x08 /* Private */,
+      17,    0,  223,    2, 0x08 /* Private */,
+      18,    0,  224,    2, 0x08 /* Private */,
+      19,    0,  225,    2, 0x08 /* Private */,
+      20,    0,  226,    2, 0x08 /* Private */,
+      21,    0,  227,    2, 0x08 /* Private */,
+      22,    0,  228,    2, 0x08 /* Private */,
+      23,    0,  229,    2, 0x08 /* Private */,
+      24,    0,  230,    2, 0x08 /* Private */,
+      25,    0,  231,    2, 0x08 /* Private */,
+      26,    0,  232,    2, 0x08 /* Private */,
+      27,    0,  233,    2, 0x08 /* Private */,
+      28,    0,  234,    2, 0x08 /* Private */,
+      29,    0,  235,    2, 0x08 /* Private */,
+      30,    0,  236,    2, 0x08 /* Private */,
+      31,    1,  237,    2, 0x08 /* Private */,
+      33,    1,  240,    2, 0x08 /* Private */,
+      35,    1,  243,    2, 0x08 /* Private */,
+      36,    1,  246,    2, 0x08 /* Private */,
+      37,    1,  249,    2, 0x08 /* Private */,
+      38,    1,  252,    2, 0x08 /* Private */,
+      39,    1,  255,    2, 0x08 /* Private */,
+      40,    1,  258,    2, 0x08 /* Private */,
+      42,    1,  261,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Bool,    3,
@@ -122,15 +161,31 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void, QMetaType::Int,   11,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   15,
-    QMetaType::Void, QMetaType::Int,   17,
-    QMetaType::Void, QMetaType::Int,   17,
-    QMetaType::Void, QMetaType::Int,   17,
-    QMetaType::Void, QMetaType::Int,   17,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   32,
+    QMetaType::Void, QMetaType::Int,   34,
+    QMetaType::Void, QMetaType::Int,   34,
+    QMetaType::Void, QMetaType::Int,   34,
     QMetaType::Void, QMetaType::Bool,    3,
     QMetaType::Void, QMetaType::Bool,    3,
     QMetaType::Void, QMetaType::Bool,    3,
-    QMetaType::Void, QMetaType::Int,   25,
+    QMetaType::Void, QMetaType::Int,   41,
     QMetaType::Void, QMetaType::Bool,    3,
 
        0        // eod
@@ -150,18 +205,34 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 5: _t->on_toggleLSB_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
         case 6: _t->on_toggleRun_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
         case 7: _t->on_spinFreq_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 8: _t->tmrRead_timeout(); break;
-        case 9: _t->setShift(); break;
-        case 10: _t->on_shiftChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 11: _t->on_spinOffset_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 12: _t->on_spinCenter_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 13: _t->on_spinSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 14: _t->on_RTLGSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 15: _t->on_RTLHAGC_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 16: _t->on_RTLSAGC_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 17: _t->on_UPConv_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
-        case 18: _t->on_comboDirectSamp_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 19: _t->on_toggleTransmit_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 8: _t->on_VFOH_pressed(); break;
+        case 9: _t->on_VFOK_pressed(); break;
+        case 10: _t->on_VFOM_pressed(); break;
+        case 11: _t->on_VFOG_pressed(); break;
+        case 12: _t->on_FQH_pressed(); break;
+        case 13: _t->on_FQK_pressed(); break;
+        case 14: _t->on_FQM_pressed(); break;
+        case 15: _t->on_FQG_pressed(); break;
+        case 16: _t->on_OFH_pressed(); break;
+        case 17: _t->on_OFK_pressed(); break;
+        case 18: _t->on_OFM_pressed(); break;
+        case 19: _t->on_FP100_pressed(); break;
+        case 20: _t->on_FP10_pressed(); break;
+        case 21: _t->on_FP1_pressed(); break;
+        case 22: _t->on_FM100_pressed(); break;
+        case 23: _t->on_FM10_pressed(); break;
+        case 24: _t->on_FM1_pressed(); break;
+        case 25: _t->tmrRead_timeout(); break;
+        case 26: _t->setShift(); break;
+        case 27: _t->on_shiftChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 28: _t->on_spinOffset_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 29: _t->on_spinCenter_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 30: _t->on_RTLGSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 31: _t->on_RTLHAGC_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 32: _t->on_RTLSAGC_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 33: _t->on_UPConv_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 34: _t->on_comboDirectSamp_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 35: _t->on_toggleTransmit_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -192,13 +263,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 20)
+        if (_id < 36)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 20;
+        _id -= 36;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 20)
+        if (_id < 36)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 20;
+        _id -= 36;
     }
     return _id;
 }
